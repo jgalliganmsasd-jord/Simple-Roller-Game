f6bcb45e6a08b79c323a9956ceb9ec57e46a575f
jgalliganmsasd-jord <jgalligan_msasd@magnoliascience.org>
2026-09-17 12:00:02 -0700

    done

--- js/config.js
+++ js/config.js
@@ -19,7 +19,7 @@ var CONFIG = {
 
   // --- how the player moves -------------------------------------------
   MOVE_SPEED: 3.2,      // pixels per frame left and right
-  JUMP_POWER: 26.6,     // how hard the jump pushes UP. bigger = higher
+  JUMP_POWER: 15.6,     // how hard the jump pushes UP. bigger = higher
   GRAVITY: 0.62,       // how hard the world pulls DOWN. bigger = heavier
   MAX_FALL: 16,       // fastest the player is allowed to fall
 
